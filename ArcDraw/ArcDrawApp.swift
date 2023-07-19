8e8226c39252a211cb2f789fd5f2fa8565b4217b
Bruce Johnson <brucehjohnson@comcast.net>
2023-07-19 14:47:43 -0500

    Initial Commit

---
+++ ArcDraw/ArcDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcDrawApp.swift
+//  ArcDraw
+//
+//  Created by Bruce Johnson on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ArcDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
